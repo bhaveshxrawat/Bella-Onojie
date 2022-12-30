7e4d3a5e195319bdb1ac25f7a248ad0b62b71a61
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2022-12-31 01:56:27 +0530

    added responsive and hamburger functionality to the btn

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-// const mediaQuery = window.matchMedia("(max-width:678px)"); // define the media query
-// const header = document.querySelector("header"); // select the header element
-// const hamBtn = document.createElement("button"); // create a button element
-// hamBtn.setAttribute('id', 'ham-Btn'); // set the id of the button to "ham-Btn"
-// mediaQuery.onchange = () => { // listen for changes to the media query
-//   if (mediaQuery.matches) { // if the media query matches
-//     header.appendChild(hamBtn); // append the button to the header element
-//   } else if (header.contains(hamBtn)) { // if the media query does not match and the header element contains the button
-//     hamBtn.remove(); // remove the button from the header element
-//   }
-// };
-const hamBtn = document.getElementById('ham-Btn')
-const hiddenNav = document.querySelector('.hidden_nav')
+const hamBtn = document.getElementById("ham-Btn");
+const hiddenNav = document.querySelector(".nav_header");
 hamBtn.onclick = () => {
-  hiddenNav.classList.toggle('active')
-}
+  hiddenNav.classList.toggle("active");
+  document.body.classList.toggle('openDrawer');
+};
+const phoneIll = document.querySelector(".phone_illustrations");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("onscreen", entry.isIntersecting);
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target)
+    }
+  });
+},
+{
+  threshold: 0.9,
+});
+
+observer.observe(phoneIll);
